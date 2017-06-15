8b6d44dfd152f043e6012a4bf70e70a0779ef48e
AITAKLI Sofiane <sofiane.aitakli@hotmail.com>
2017-06-15 21:47:07 +0100

    Add test for segment unit

--- AgodaFileDownloader.UTest/UTDownloadFeatures.cs
+++ AgodaFileDownloader.UTest/UTDownloadFeatures.cs
@@ -17,6 +17,7 @@ namespace AgodaFileDownloader.UTest
     public class UtDownloadFeature
     {
         UnityContainer container = new UnityContainer();
+
         [TestInitialize]
         public void InitialMethod()
         {
@@ -41,16 +42,15 @@ namespace AgodaFileDownloader.UTest
                    "ftp://150.140.208.250/share/ebooks/COMICS%20-%20The%20Ultimate%20Collection/%CE%A4%CE%B5%CF%8D%CF%87%CE%BF%CF%82%20252%20-%20%CE%97%20%CE%A7%CE%B1%CE%BC%CE%AD%CE%BD%CE%B7%20%CE%9C%CE%BF%CF%8D%CE%BC%CE%B9%CE%B1.pdf",
                      "ftp://150.140.208.250/share/ebooks/COMICS%20-%20The%20Ultimate%20Collection/%CE%A4%CE%B5%CF%8D%CF%87%CE%BF%CF%82%20258%20-%20%CE%A4%CE%B1%CE%BE%CE%AF%CE%B4%CE%B9%20%CE%9C%CE%B5%20%CE%A4%CE%BF%CE%BD%20%CE%9A%CE%BF%CE%BB%CF%8C%CE%BC%CE%B2%CE%BF.pdf",
                      "ftp://150.140.208.250/share/ebooks/COMICS%20-%20The%20Ultimate%20Collection/%CE%A4%CE%B5%CF%8D%CF%87%CE%BF%CF%82%20272%20-%20%CE%A3%CF%84%CE%B7%CE%BD%20%CE%9C%CE%AD%CF%83%CE%B7%20%CE%A4%CE%BF%CF%85%20%CE%A0%CE%BF%CF%85%CE%B8%CE%B5%CE%BD%CE%AC.pdf",
-                     "ftp://150.140.208.250/share/ebooks/COMICS%20-%20The%20Ultimate%20Collection/%CE%A4%CE%B5%CF%8D%CF%87%CE%BF%CF%82%20044%20-%20%CE%A0%CE%B1%CE%B3%CF%89%CE%BC%CE%AD%CE%BD%CE%BF%20%CF%87%CF%81%CF%85%CF%83%CE%AC%CF%86%CE%B9.pdf",
-                     "http://clicnet.swarthmore.edu/boudjedra.pdf",
+                      "http://clicnet.swarthmore.edu/boudjedra.pdf",
                      "http://www.gideonphoto.com/blog/wp-content/uploads/2012/12/IMG_8940_2.jpg",
+                    "ftp://150.140.208.250/share/ebooks/COMICS%20-%20The%20Ultimate%20Collection/%CE%A4%CE%B5%CF%8D%CF%87%CE%BF%CF%82%20044%20-%20%CE%A0%CE%B1%CE%B3%CF%89%CE%BC%CE%AD%CE%BD%CE%BF%20%CF%87%CF%81%CF%85%CF%83%CE%AC%CF%86%CE%B9.pdf",
+                    
                      },
                 new List<AuthenticatedUrl>());
             downloadManger.Download();
         }
-
-
-
+        
         [TestMethod]
         public void IsSupportingMultiThreadingForMultipleFiles()
         {
@@ -65,8 +65,7 @@ namespace AgodaFileDownloader.UTest
                 new List<AuthenticatedUrl>());
             downloadManger.Download();
         }
-
-
+        
         [TestMethod]
         public void IsSupportingMultiThreadingForMultipleSegments()
         {
@@ -81,9 +80,7 @@ namespace AgodaFileDownloader.UTest
                 new List<AuthenticatedUrl>());
             downloadManger.Download();
         }
-
-
-
+        
         [TestMethod]
         public void IsDeletingIfIncomplete()
         {
@@ -96,8 +93,7 @@ namespace AgodaFileDownloader.UTest
                 new List<AuthenticatedUrl>());
             downloadManger.Download();
         }
-
-
+        
         [TestMethod]
         public void IsFailingIfGetInfoFails()
         {
@@ -106,7 +102,18 @@ namespace AgodaFileDownloader.UTest
                 "ftp://cda.cfa.harvard.edu/pub/.snapshot/weekly.2017-05-21_0015/science/ao16/cat7/18745/secondary/acisf18745_000N001_evt1.fits.gz",
             */
         }
+        
+        [TestMethod]
+        public void IsSegmentFailsAfterMaxRetries()
+        {
 
+        }
+
+        [TestMethod]
+        public void IsSegmentSupportRetries()
+        {
+
+        }
 
     }
 }
--- AgodaFileDownloader.UTest/UtIOSegments.cs
+++ AgodaFileDownloader.UTest/UtIOSegments.cs
@@ -1,38 +1,197 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using AgodaFileDownloader.Model;
+using AgodaFileDownloader.Service;
+using AgodaFileDownloader.Service.ServiceInterface;
+using AgodaFileDownloader.Helper;
+using AgodaFileDownloader.Service.ServiceImplementaion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace AgodaFileDownloader.UTest
 {
     [TestClass]
     public class UtIOSegments
     {
+        Mock<IProtocolDownloader> _protocolDownloadSuccessMock = new Mock<IProtocolDownloader>();
+        Mock<IProtocolDownloader> _protocolFailureMock = new Mock<IProtocolDownloader>();
+        private MemoryStream _testInStream=new MemoryStream();
+        private long _testStreamLength;
+        Segment _segment = new Segment();
+        Segment _segmentCheckPosition = new Segment();
+        ResourceDetail resourceDetail = new ResourceDetail();
+        RemoteFileDetail remoteFileInfo=new RemoteFileDetail();
+        [TestInitialize]
+        public void Initialize()
+        {
+            MockDataCreation();
+            
+        }
+
         [TestMethod]
         public void IsSegmentWritten()
         {
-            
+            var processor = new SegmentProcessor();
+            var result=processor.ProcessSegment(resourceDetail, _protocolDownloadSuccessMock.Object, _segment);
+            Assert.IsFalse(result.Denied);
+            Assert.IsNotNull(_segment.OutputStream);
+            Assert.AreEqual(_segment.StartPosition, _segment.EndPosition);
+            string valueWritten;
+            _segment.OutputStream.Position = 0;
+            using (StreamReader reader = new StreamReader(_segment.OutputStream))
+            {
+                valueWritten=reader.ReadToEnd();
+            }
+          
+            Assert.AreEqual(valueWritten, "a test string for the stream");
+            _segment.OutputStream.Close();
+        }
+
+
+        [TestMethod]
+        public void IsSegmentStreamCreationSupportFail()
+        {
+            var processor = new SegmentProcessor();
+            ResponseBase result = processor.ProcessSegment(resourceDetail, _protocolFailureMock.Object, _segment);
+            Assert.IsTrue(result.Denied);
+            Assert.AreEqual(_segment.CurrentTry, 1);
+            Assert.AreEqual(_segment.StartPosition, 0);
+
+        }
+        
+        [TestMethod]
+        public void IsSegmentStreamWritingSupportFail()
+        {
+            var processor = new SegmentProcessor();
+            _segment.OutputStream = null;
+            ResponseBase result = processor.ProcessSegment(resourceDetail, _protocolDownloadSuccessMock.Object, _segment);
+            Assert.IsTrue(result.Denied);
+            Assert.AreEqual(_segment.CurrentTry,1);
+            Assert.AreEqual(_segment.StartPosition, 0);
+        }
+
+        [TestMethod]
+        public void IsStartedPositionMoved()
+        {
+            var processor = new SegmentProcessor();
+            _segmentCheckPosition.OutputStream = new MemoryStream();
+            var result = processor.ProcessSegment(resourceDetail, _protocolDownloadSuccessMock.Object, _segmentCheckPosition);
+            Assert.IsFalse(result.Denied);
+            Assert.IsNotNull(_segmentCheckPosition.OutputStream);
+            Assert.AreEqual(_segmentCheckPosition.OutputStream.Position, _testStreamLength);
+            _segmentCheckPosition.OutputStream.Close();
+
         }
 
         [TestMethod]
         public void IsFileSplitToSegments()
         {
-            
+            var processor = new SegmentProcessor();
+            int numberOfSegments = 10;
+            remoteFileInfo.FileSize = 2500000;
+            var listSegmentResponse = processor.GetSegments(numberOfSegments, remoteFileInfo, "FileName");
+            Assert.IsFalse(listSegmentResponse.Denied);
+            Assert.IsNotNull(listSegmentResponse.ReturnedValue);
+            var listSegment = listSegmentResponse.ReturnedValue;
+            Assert.AreEqual(listSegment.Count,10);
+            long startPosition = 0;
+            long endPosition = 250000;
+            foreach (Segment t in listSegment)
+            {
+                Assert.AreEqual(t.StartPosition, startPosition);
+                Assert.AreEqual(t.EndPosition, endPosition);
+                startPosition = endPosition;
+                endPosition=endPosition+ remoteFileInfo.FileSize/10;
+            }
         }
 
+        [TestMethod]
+        public void IsFileSplitSingleIfInferiorToMiSize()
+        {
+            var processor = new SegmentProcessor();
+            int numberOfSegments = 10;
+            remoteFileInfo.FileSize = 170000;
+            var listSegmentResponse = processor.GetSegments(numberOfSegments, remoteFileInfo, "FileName");
+            Assert.IsFalse(listSegmentResponse.Denied);
+            Assert.IsNotNull(listSegmentResponse.ReturnedValue);
+            var listSegment = listSegmentResponse.ReturnedValue;
+            Assert.AreEqual(listSegment.Count, 1);
+            long startPosition = 0;
+            long endPosition = 170000;
+
+            Assert.AreEqual(listSegment[0].StartPosition, startPosition);
+            Assert.AreEqual(listSegment[0].EndPosition, endPosition);
+           
+        }
+        
+        [TestMethod]
+        public void IsFileSplitInferiorToMiSize()
+        {
+            var processor = new SegmentProcessor();
+            int numberOfSegments = 10;
+            remoteFileInfo.FileSize = 230000;
+            var listSegmentResponse = processor.GetSegments(numberOfSegments, remoteFileInfo, "FileName");
+            Assert.IsFalse(listSegmentResponse.Denied);
+            Assert.IsNotNull(listSegmentResponse.ReturnedValue);
+            var listSegment = listSegmentResponse.ReturnedValue;
+            Assert.AreEqual(listSegment.Count, 1);
+            long startPosition = 0;
+            long endPosition = 230000;
+
+            Assert.AreEqual(listSegment[0].StartPosition, startPosition);
+            Assert.AreEqual(listSegment[0].EndPosition, endPosition);
+
+        }
+        
         [TestMethod]
         public void IsSegmentSupportErrors()
         {
             
         }
-
-        [TestMethod]
-        public void IsSegmentFailsAfterMaxRetries()
+        
+        void MockDataCreation()
         {
+            #region initSuccessProtocolDownloader
+            _testInStream = new MemoryStream(Encoding.UTF8.GetBytes("a test string for the stream"));
+            MemoryStream testOutStream = new MemoryStream();
+            _testStreamLength= _testInStream.Length;
+            _segmentCheckPosition.EndPosition = _testStreamLength;
+            _segment.OutputStream = testOutStream;
+            _segment.EndPosition = _testInStream.Length;
+            _protocolDownloadSuccessMock.Setup(e => e.GetFileInfo(It.IsAny<ResourceDetail>()))
+               .Returns(() => new ResponseBase<RemoteFileDetail>()
+               {
+                   Denied = false,
+                   ReturnedValue = new RemoteFileDetail()
+                   {
+                       
+                   }
+               });
+            _protocolDownloadSuccessMock.Setup(e => e.CreateStream(It.IsAny<ResourceDetail>(), It.IsAny<long>(), It.IsAny<long>()))
+                .Returns(() => new ResponseBase<Stream>()
+                {
+                    Denied = false,
+                    ReturnedValue = _testInStream
+                });
+            #endregion
 
-        }
+            _protocolFailureMock.Setup(e => e.GetFileInfo(It.IsAny<ResourceDetail>()))
+               .Returns(() => new ResponseBase<RemoteFileDetail>()
+               {
+                   Denied = false,
+                   ReturnedValue = new RemoteFileDetail()
+                   {
 
-        [TestMethod]
-        public void IsSegmentSupportRetries()
-        {
-
+                   }
+               });
+            _protocolFailureMock.Setup(e => e.CreateStream(It.IsAny<ResourceDetail>(), It.IsAny<long>(), It.IsAny<long>()))
+                .Returns(() => new ResponseBase<Stream>()
+                {
+                    Denied = true,
+                    ReturnedValue = null
+                });
+            
         }
     }
 }
