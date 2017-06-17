508e19d408cc97827ab41b907efdcb76075e2832
AITAKLI Sofiane <sofiane.aitakli@hotmail.com>
2017-06-17 21:50:08 +0100

    move path helper to the helper project

---
+++ AgodaFileDownloader.Helper/PathHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace AgodaFileDownloader.Helper
+{
+    public static class PathHelper
+    {
+        public static string GetWithBackslash(string path)
+        {
+            if (!path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                path += Path.DirectorySeparatorChar.ToString();
+            }
+
+            return path;
+
+        }
+    }
+}
